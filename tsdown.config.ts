a6ce355aa04f5e8360ccdc04650f5cdc165cda4c
yasintz <yasinntazeoglu@gmail.com>
2025-12-23 15:35:31 +0300

    tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,46 @@
+import { defineConfig } from 'tsdown';
+
+export default defineConfig([
+  {
+    entry: ['src/index.tsx'],
+    format: 'cjs',
+    dts: true,
+    sourcemap: true,
+    clean: true,
+    external: [
+      'react',
+      'react-dom',
+      '@radix-ui/react-dialog',
+      '@radix-ui/react-select',
+      '@radix-ui/react-slot',
+    ],
+    treeshake: true,
+    outExtensions() {
+      return {
+        js: '.js',
+        dts: '.d.ts',
+      };
+    },
+  },
+  {
+    entry: ['src/index.tsx'],
+    format: 'esm',
+    dts: true,
+    sourcemap: true,
+    external: [
+      'react',
+      'react-dom',
+      '@radix-ui/react-dialog',
+      '@radix-ui/react-select',
+      '@radix-ui/react-slot',
+    ],
+    treeshake: true,
+    outExtensions() {
+      return {
+        js: '.mjs',
+        dts: '.d.ts',
+      };
+    },
+  },
+]);
+
